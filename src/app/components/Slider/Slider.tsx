0be1cb35c2bee0a92aece223262a43e750a6d525
leon_2192 <leon.strokes@gmail.com>
2023-09-28 16:26:49 -0300

    fix typical

--- src/app/components/Slider/Slider.tsx
+++ src/app/components/Slider/Slider.tsx
@@ -1,16 +1,8 @@
 "use client";
 import React from "react";
-import { motion } from "framer-motion";
-import Typical from "react-typical";
-
 const Slider = () => {
-  const textVariants = {
-    hidden: { x: -100 },
-    visible: { x: 0, transition: { duration: 1 } },
-  };
-
   return (
-    <motion.div
+    <div
       className="flex items-center w-[100%] h-[700px] bg-cover bg-center relative"
       style={{ backgroundImage: "url('/bannerhome2.jpg')" }}
     >
@@ -18,52 +10,32 @@ const Slider = () => {
       <div className="p-6 mt-40">
         <br />
         <br />
-        <motion.h2
+        <h2
           className="text-white text-6xl font-bold mt-20"
           style={{
             color: "#90e0ef",
           }}
-          variants={textVariants}
-          initial="hidden"
-          animate="visible"
         >
           Nahitan
-        </motion.h2>
-        <motion.h2
+        </h2>
+        <h2
           className="text-white text-5xl font-bold mt-5"
           style={{ color: "#f72585" }}
-          variants={textVariants}
-          initial="hidden"
-          animate="visible"
         >
           Diseño y Desarrollo Web
-        </motion.h2>
+        </h2>
         <br />
-        <motion.h4
-          className="text-white text-4xl font-bold"
+        <h4
+          className="text-white text-3xl font-bold"
           style={{
-            width: "100%",
+            width: "75%",
             color: "#fcf6bd",
           }}
-          variants={textVariants}
-          initial="hidden"
-          animate="visible"
         >
-          <Typical
-            steps={[
-              "Damos vida a tu proyecto",
-              1000,
-              "Impulsamos tus ideas",
-              1000,
-              "Hacemos que tu negocio ¡DESPEGUE! 🚀",
-              1000,
-            ]}
-            loop={Infinity}
-            wrapper="p"
-          />
-        </motion.h4>
+          Damos vida a tu proyecto
+        </h4>
       </div>
-    </motion.div>
+    </div>
   );
 };
 
